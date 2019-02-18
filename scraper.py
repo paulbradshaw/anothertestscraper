b1157105225f97d8304ec7010af6441f298c148f
Paul Bradshaw <paulonhismobile@gmail.com>
2019-02-18 14:47:25 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -20,7 +20,7 @@ print(footers)
 for footer in footers:
   footertextall = footer.text_content()
   print(len(footertextall))
-  print(footertextall[0:15])
+  print(footertextall[0:20])
   print(footer)
   convertedfooter = lxml.html.tostring(footer)
   print(convertedfooter)
